62c8e1263b07af073fc711c1c88ff7597ec96884
mtmzorro <mtmzorro@foxmail.com>
2020-06-20 19:04:56 +0800

    add GA

--- config-overrides.js
+++ config-overrides.js
@@ -1,20 +1,26 @@
-// react-app-rewired 
+// react-app-rewired
 // https://github.com/timarney/react-app-rewired/blob/master/README_zh.md
-const CopyPlugin = require('copy-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = function override(config, env) {
     // ENV development on yarn start
     // if (env === 'development') {
     // }
     // ENV production on yarn build
-    if (env === 'production') {
+    if (env === "production") {
         // add CopyPlugin
         config.plugins.push(
             new CopyPlugin([
-                { from: 'src/content-scripts', to: 'chrome-assets/content-scripts' },
-                { from: 'src/config/config.js', to: 'chrome-assets/content-scripts' }
+                {
+                    from: "src/content-scripts",
+                    to: "chrome-assets/content-scripts",
+                },
+                {
+                    from: "src/config/config.js",
+                    to: "chrome-assets/content-scripts",
+                },
             ])
-        )
+        );
     }
     return config;
-}
+};
